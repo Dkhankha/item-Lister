8b9fb875334e04d3a3ca71b6a0993f61872a97db
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-07-30 13:29:44 +0530

    js file_4

---
+++ assgn_4.js
@@ -0,0 +1,6 @@
+let item= document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor = 'green';
+let itemBold =document.getElementsByClassName('list-group-item');
+for(let i=0;i<itemBold.length; i++){
+itemBold[i].style.fontWeight = 'bold';
+}
