13eaf76000f05177f0ae276b83889c79d30e3dc3
Dkhankha <140619121+Dkhankha@users.noreply.github.com>
2023-08-11 18:31:17 +0530

    file with delete button

--- danyl.js
+++ danyl.js
@@ -34,32 +34,59 @@
 //     document.querySelector('body').classList.add('bg-dark');
 // })
 
-let form=document.getElementById('my-form');
-let nameInput=document.getElementById('name');
-let emailInput=document.getElementById('email');
-let msg=document.getElementById('msg');
-let userList=document.querySelector('#users');
-form.addEventListener('submit', onSubmit);
-    function onSubmit(e){
-    e.preventDefault();
+// let form=document.getElementById('my-form');
+// let nameInput=document.getElementById('name');
+// let emailInput=document.getElementById('email');
+// let msg=document.getElementById('msg');
+// let userList=document.querySelector('#users');
+// form.addEventListener('submit', onSubmit);
+//     function onSubmit(e){
+//     e.preventDefault();
     
-    if(nameInput.value === '' || emailInput.value === ''){
-        msg.classList.add('error');
-        msg.innerHTML='Please enter the fields'
-    }else{
-       let li = document.createElement('li');
-       li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-       userList.appendChild(li);
-       //let user_records=[];
-       user_records=JSON.parse(localStorage.getItem('users'))?JSON.parse(localStorage.getItem('users')):{}
-       user_records={
-        'name':nameInput.value,
-        'email':emailInput.value
-       }
+//     if(nameInput.value === '' || emailInput.value === ''){
+//         msg.classList.add('error');
+//         msg.innerHTML='Please enter the fields'
+//     }else{
+//        let li = document.createElement('li');
+//        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+//        userList.appendChild(li);
+//        //let user_records=[];
+//        user_records=JSON.parse(localStorage.getItem('users'))?JSON.parse(localStorage.getItem('users')):{}
+//        user_records={
+//         'name':nameInput.value,
+//         'email':emailInput.value
+//        }
 
-       localStorage.setItem('users',JSON.stringify(user_records))
-       nameInput.value='';
-       emailInput.value='';
-    }
+//        localStorage.setItem('users',JSON.stringify(user_records))
+//        nameInput.value='';
+//        emailInput.value='';
+//     }
+// }
+
+function saveData(event){
+    event.preventDefault()
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
+    let obj={
+        "name":name,
+        "email":email
+    }  
+    localStorage.setItem(obj.email,JSON.stringify(obj))
+    showUsers(obj)
 }
-
+function showUsers(obj){
+    let parentElem=document.getElementById('users');
+    let childelem =document.createElement('li');
+    childelem.appendChild(document.createTextNode(`${obj.name}: ${obj.email}`))
+    parentElem.appendChild(childelem);
+    
+    let deletebutton=document.createElement('input');
+    deletebutton.type='button';
+    deletebutton.value='delete';
+    deletebutton.onclick=()=>{
+        localStorage.removeItem(obj.email)
+        parentElem.removeChild(childelem)
+    }
+    childelem.appendChild(deletebutton)
+    parentElem.appendChild(childelem)
+}
